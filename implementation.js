25c18516152822085831b2f08d327b40b6b3ee66
Jordan Harband <ljharb@gmail.com>
2019-12-14 20:48:13 -0800

    [Refactor] only use `global` in node; only check browser globals in browsers
    Fixes #2.

--- implementation.js
+++ implementation.js
@@ -1,13 +1,3 @@
-/* eslint no-negated-condition: 0, no-new-func: 0 */
-
 'use strict';
 
-if (typeof self !== 'undefined') {
-	module.exports = self;
-} else if (typeof window !== 'undefined') {
-	module.exports = window;
-} else if (typeof global !== 'undefined') {
-	module.exports = global;
-} else {
-	module.exports = Function('return this')();
-}
+module.exports = global;
