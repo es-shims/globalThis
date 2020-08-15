f1e709d80f14f49ab17adb95dfb9448845ac24a9
bluelovers <codelovers@users.sourceforge.net>
2020-08-15 15:52:39 +0800

    Update polyfill.js

--- polyfill.js
+++ polyfill.js
@@ -1,10 +1,8 @@
 'use strict';
 
-var implementation = require('./implementation');
-
 module.exports = function getPolyfill() {
 	if (typeof global !== 'object' || !global || global.Math !== Math || global.Array !== Array) {
-		return implementation;
+		return require('./implementation');
 	}
 	return global;
 };
