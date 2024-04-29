0209ccb2cd95b785e7e8868fab035cdc87216b58
Jordan Harband <ljharb@gmail.com>
2024-04-29 13:51:31 -0700

    [Refactor] use `gopd`

--- shim.js
+++ shim.js
@@ -1,13 +1,20 @@
 'use strict';
 
 var define = require('define-properties');
+var gOPD = require('gopd');
 var getPolyfill = require('./polyfill');
 
 module.exports = function shimGlobal() {
 	var polyfill = getPolyfill();
 	if (define.supportsDescriptors) {
-		var descriptor = Object.getOwnPropertyDescriptor(polyfill, 'globalThis');
-		if (!descriptor || (descriptor.configurable && (descriptor.enumerable || !descriptor.writable || globalThis !== polyfill))) { // eslint-disable-line max-len
+		var descriptor = gOPD(polyfill, 'globalThis');
+		if (
+			!descriptor
+			|| (
+				descriptor.configurable
+				&& (descriptor.enumerable || !descriptor.writable || globalThis !== polyfill)
+			)
+		) {
 			Object.defineProperty(polyfill, 'globalThis', {
 				configurable: true,
 				enumerable: false,
